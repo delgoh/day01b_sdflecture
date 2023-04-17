09d4983484a648b22b3ce02667e8f68415b7fee8
Degojix <51292776+delgoh@users.noreply.github.com>
2023-04-17 17:11:11 +0800

    Slide 33

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,5 +1,9 @@
 package sg.edu.nus.iss;
 
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Console cons = System.console();
+
+        List<String> todos = new ArrayList<>();
+
+        String input = "";
+        while (!input.equals("quit")) {
+            input = cons.readLine("Enter a string: ");
+            if (!input.equals("quit"))
+                todos.add(input);
+
+            System.out.println("Current task list:");
+            int index = 0;
+            for (String todo: todos)
+                System.out.println("Task " + (++index) + ": " + todo);
+        }
     }
 }
